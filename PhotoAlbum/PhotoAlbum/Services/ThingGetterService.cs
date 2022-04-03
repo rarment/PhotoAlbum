f0ceebb1f7a48918d98105c3d5130d51e0055e70
Ryan Arment <ryan.arment@gmail.com>
2022-04-03 16:39:25 -0500

    unused usings

--- PhotoAlbum/PhotoAlbum/Services/ThingGetterService.cs
+++ PhotoAlbum/PhotoAlbum/Services/ThingGetterService.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using PhotoAlbum.Models;
-using System.Net.Http;
-using System.Net.Http.Json;
 using PhotoAlbum.Repos;
 using Serilog;
 
